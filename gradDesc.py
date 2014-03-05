06c207370133062afcd0f3e56cce424b833c2a55
Arun Drelich <ad2476@gmail.com>
2014-03-05 08:55:51 -0500

    Exception handling for overflow - checks divergence

--- gradDesc.py
+++ gradDesc.py
@@ -3,7 +3,18 @@
 import shuntingYard
 from shuntingYard import *
 
+import sys
+
+# F(x,y)=x*x*x+2*x+6*x*y-3*y*y
+# grad-F=3*x*x+2+6*y, 6*x-6*y
+
 # This will run gradient descent/ascent on a function F(x,y) from a given 2-D gradient
+def computeCost(function, theta):
+	try:
+		return eval(function, theta)
+	except OverflowError:
+		return sys.float_info.max
+
 
 # dplane = [(x,y), (x,y), (x,y)]
 # theta[j] := theta[j]-alpha*(dF/dtheta[j])
@@ -14,25 +25,30 @@ def gradientDescent(gradient, coords, alpha, num_iters):
 	for i in xrange(len(theta)):
 		theta[i]=float(theta[i])
 
-	# 1. Compute partial derivatives (evaluate gradient at each theta step)
-	# 2. Update both thetas simultaneously
-	for i in xrange(num_iters):
-		temp=[] # hold transitionary thetas
-		dtheta=[eval(grad[0], theta), eval(grad[1], theta)]
+	try:
+		# 1. Compute partial derivatives (evaluate gradient at each theta step)
+		# 2. Update both thetas simultaneously
+		for i in xrange(num_iters):
+			temp=[] # hold transitionary thetas
+			dtheta=[eval(grad[0], theta), eval(grad[1], theta)]
+			#print "\t"+str(dtheta)
 
-		# Update theta simultaneously => hold transitional thetas in temp
-		for j, theta_j in enumerate(theta):
-			theta_j=theta_j - alpha*dtheta[j]
-			temp.append(theta_j)
+			# Update theta simultaneously => hold transitional thetas in temp
+			for j, theta_j in enumerate(theta):
+				theta_j=theta_j - alpha*dtheta[j]
+				temp.append(theta_j)
 
-		theta=temp # update theta simultaneously
-		#print theta # May want to disable this debugging since num_iters=1000...
+			theta=temp # update theta simultaneously
+			#print theta # May want to disable this debugging since num_iters=1000...
 
-	# Round off theta to 5 decimal points
-	for i in xrange(len(theta)):
-		theta[i]=round(theta[i], 5)
+		# Round off theta to 5 decimal points
+		for i in xrange(len(theta)):
+			theta[i]=round(theta[i], 5)
 
-	return tuple(theta)
+		return tuple(theta)
+	except OverflowError:
+		print "Overflow. Algorithm diverges!"
+		return (sys.float_info.max, sys.float_info.max)
 
 if __name__ == '__main__':
 	import main
